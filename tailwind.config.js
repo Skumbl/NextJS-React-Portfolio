5ebf7ad8fa3e41791c4af9e83a06ca076144eeea
Skumbl <janarvik01@gmail.com>
2022-11-27 16:21:53 -0500

    added reactive elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -8,6 +9,7 @@ module.exports = {
     extend: {
       fontFamily: {
         courier: "Courier",
+        courierBold: "Courier-Bold",
       }
     },
   },
